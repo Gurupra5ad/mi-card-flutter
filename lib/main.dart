71a431298bf3ec2d67f866dfd76da663cef106b9
Gurupra5ad <guruprasad2511@gmail.com>
2023-08-13 22:46:17 +0530

    initializing business card app

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,26 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(myApp());
+}
+
+class myApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+          child: Column(
+            children:<Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/Guru.jpeg'),
+              )
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
+
