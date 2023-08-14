bf4af04b1f783275826b6f62ed3c763846871b10
Gurupra5ad <guruprasad2511@gmail.com>
2023-08-14 16:10:04 +0530

    v1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,82 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(myApp());
+  runApp(MyApp());
 }
 
-class myApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children:<Widget>[
               CircleAvatar(
                 radius: 50,
                 backgroundImage: AssetImage('images/Guru.jpeg'),
-              )
+              ),
+              Text('Guru Prasad',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40,
+                  fontWeight: FontWeight.w400,
+                  color: Colors.white,
+                ),
+              ),
+              Text( 'FLUTTER DEVELOPER',
+                style:TextStyle(
+                  fontFamily: 'Source Sans',
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.teal[100]
+                ),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal[100],
+                ),
+              ),
+              Card(
+                color:Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 20, horizontal: 20),
+                child: ListTile(
+                  leading:Icon(
+                    Icons.call,
+                    size: 25,
+                    color: Colors.teal[900],
+                  ),
+                  title:Text('+91 99949 87315',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans',
+                      color: Colors.teal[800],
+                      fontSize: 20,
+                      fontWeight: FontWeight.w100,
+                    ),
+                ),
+              )),
+              Card(
+                color:Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+                child: ListTile(
+                  leading:Icon(
+                    Icons.email,
+                    size: 25,
+                    color: Colors.teal[900],
+                  ),
+                  title:Text('guru@mechonyx.com',
+                    style: TextStyle(
+                      fontFamily: 'Source Sans',
+                      color: Colors.teal[800],
+                      fontSize: 20,
+                      fontWeight: FontWeight.w100,
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
@@ -23,4 +84,3 @@ class myApp extends StatelessWidget {
     );
   }
 }
-
